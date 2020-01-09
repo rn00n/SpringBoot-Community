934a689d89a4fec8a9c7c02cf6a58d76fd39279d
rn00n <rn00n@naver.com>
2020-01-09 14:49:11 +0900

    add svn setting

--- community/src/main/java/com/havi/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.havi.repository;
-
-import com.havi.domain.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-}
--- community/src/main/java/com/havi/service/BookService.java
+++
@@ -1,9 +0,0 @@
-package com.havi.service;
-
-import com.havi.domain.Book;
-
-import java.util.List;
-
-public interface BookService {
-    List<Book> getBookList();
-}
